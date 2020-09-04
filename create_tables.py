818cccea4d7ff8adfe5358a6d14a945f5951707b
mohammad botaihan <mbt.15@hotmail.com>
2020-09-04 16:12:33 +0300

    remove auth

--- create_tables.py
+++ create_tables.py
@@ -4,7 +4,7 @@ from sql_queries import create_table_queries, drop_table_queries
 
 def create_database():
     # connect to default database
-    conn = psycopg2.connect("host=127.0.0.1 dbname=mohammad user=mohammad")
+    conn = psycopg2.connect("host=127.0.0.1 dbname= <database name> user=<username to use this database>")
     conn.set_session(autocommit=True)
     cur = conn.cursor()
     
